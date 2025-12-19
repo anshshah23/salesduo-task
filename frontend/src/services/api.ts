03998ab74b0b824648538d2fb1666ea3d4ecfa83
anshshah23 <ansh.shah082@svkmmumbai.onmicrosoft.com>
2025-12-20 00:45:13 +0530

    added ts configuration file for env resolution in the frontend folder, removed the exposed api url

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { ProductResponse } from '../types';
 
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 export const api = {
   async optimizeProduct(asin: string): Promise<ProductResponse> {
